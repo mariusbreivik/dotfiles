2625d9e4ae34aab784c6ec49ded62aefeb43afa6
Marius Breivik <marius.breivik@gmail.com>
2019-02-02 18:29:57 +0100

    update personal config

--- changePrompt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# add user and host for oh-my-zsh prompt
-
-sed -i -e 's/PROMPT=/PROMPT='%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} ${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'/g' ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
